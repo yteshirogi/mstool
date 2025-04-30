4ccdbcb672596ef0f3e8d1d210dc005daba5c700
yteshirogi <60608291+yteshirogi@users.noreply.github.com>
2025-04-30 16:07:20 +0900

    Update bilayerbuilder.py

--- src/mstool/membrane/bilayerbuilder.py
+++ src/mstool/membrane/bilayerbuilder.py
@@ -274,10 +274,7 @@ class BilayerBuilder:
 
         ### Martinize
         MartinizeDMS(workdir + '/step1.sol.dms', out = workdir + '/step1.martini.dms', martini=martiniff, addnbtype=addnbtype[0])
-        try:
-            dumpsql(workdir + '/step1.martini.dms')
-        except:
-            pass
+        dumpsql(workdir + '/step1.martini.dms')
 
         ### Create system & add posz to GL1 and GL2
         # dms = DMSFile(workdir + '/step1.martini.dms')
